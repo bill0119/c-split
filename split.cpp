cf51c0a900bcaf231e909db5e790cdbf61a73908
bill <bill@ugiltd.com>
2019-01-19 09:41:10 +0800

    string split

---
+++ split.cpp
@@ -0,0 +1,55 @@
+
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <map>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> split(const string &s, const string &seperator){
+    vector<string> result;
+    typedef string::size_type string_size;
+    string_size i = 0;
+
+    while(i != s.size()){
+        int flag = 0;
+        while(i != s.size() && flag == 0){
+            flag = 1;
+            for(string_size x = 0; x < seperator.size(); ++x)
+                if(s[i] == seperator[x]){
+                    ++i;
+                    flag = 0;
+                    break;
+                }
+        }
+    
+        flag = 0;
+        string_size j = i;
+        while(j != s.size() && flag == 0){
+            for(string_size x = 0; x < seperator.size(); ++x)
+                if(s[j] == seperator[x]){
+                    flag = 1;
+                    break;
+                }
+                if(flag == 0) 
+                    ++j;
+        }
+        if(i != j){
+            result.push_back(s.substr(i, j-i));
+            i = j;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    string S = "aa bb cc; xx yy z-z; aa bb; aa bx cc";
+    vector<string> vStr;
+    vStr = split(S, ";");
+
+    return 0;
+}
+
